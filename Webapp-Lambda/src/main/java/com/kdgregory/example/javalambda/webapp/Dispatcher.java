7cf6b6cd9e92b50038a090d4c28eea94ef061a61
Keith Gregory <contact@kdgregory.com>
2019-01-13 12:52:35 -0500

    Webapp: add request info to MDC

--- Webapp-Lambda/src/main/java/com/kdgregory/example/javalambda/webapp/Dispatcher.java
+++ Webapp-Lambda/src/main/java/com/kdgregory/example/javalambda/webapp/Dispatcher.java
@@ -18,6 +18,7 @@ import com.kdgregory.example.javalambda.webapp.util.Tokens;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 import net.sf.kdgcommons.collections.CollectionUtil;
 import net.sf.kdgcommons.lang.StringUtil;
@@ -43,6 +44,8 @@ public class Dispatcher
 
     public Map<String,Object> handler(Map<String,Object> proxyRequest, Context lambdaContext)
     {
+        MDC.put("requestId", lambdaContext.getAwsRequestId());
+
         try
         {
             // this is the only place we pull pieces out of the AWS request so forego constants
@@ -88,6 +91,9 @@ public class Dispatcher
         String action      = (String)CollectionUtil.getVia(proxyRequest, "pathParameters", "action");
         String tokenHeader = (String)CollectionUtil.getVia(proxyRequest, "headers", "Cookie");
         String body        = (String)CollectionUtil.getVia(proxyRequest, "body");
+        
+        MDC.put("requestMethod", method);
+        MDC.put("action", action);
 
         // body will be empty on GET, but rather than have separate code paths I'll give a dummy value
         if (StringUtil.isEmpty(body))
